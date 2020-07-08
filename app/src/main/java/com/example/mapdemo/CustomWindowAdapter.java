630ff9914507e8841a2e1700e8a7be18dfb63326
Patrice Gill <pgill30@fb.com>
2020-07-08 13:37:28 -0400

    Implemented getInfoContents method by inflating the layout file and binding the specific widget views

--- app/src/main/java/com/example/mapdemo/CustomWindowAdapter.java
+++ app/src/main/java/com/example/mapdemo/CustomWindowAdapter.java
@@ -1,4 +1,42 @@
 package com.example.mapdemo;
 
-public class CustomWindowAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.Marker;
+
+public class CustomWindowAdapter implements GoogleMap.InfoWindowAdapter {
+    LayoutInflater mInflater;
+
+    //constructor
+    public CustomWindowAdapter(LayoutInflater i){
+        mInflater = i;
+    }
+
+
+    //This defines the contents within the info window based on the marker
+    @Override
+    public View getInfoContents(Marker marker) {
+        //Getting the view from the layout file
+        View view = mInflater.inflate(R.layout.custom_info_window, null);
+
+        //Populate fields
+        TextView title = (TextView) view.findViewById(R.id.tv_info_window_title);
+        title.setText(marker.getTitle());
+
+        TextView description = (TextView) view.findViewById(R.id.tv_info_window_description);
+        description.setText(marker.getSnippet());
+
+        //Return info window contents
+        return view;
+    }
+
+    //This changes the frame of the info window, returning null uses the default frame
+    //This is just the border and arrow surrounding the contents specified above
+    @Override
+    public View getInfoWindow(Marker marker) {
+        return null;
+    }
 }
